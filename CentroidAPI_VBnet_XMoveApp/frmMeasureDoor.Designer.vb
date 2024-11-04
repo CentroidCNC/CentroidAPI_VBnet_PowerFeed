f8fa18276c52a992ca02e9d4f3bd83aaf3a33ed3
jholes <jholes@DESKTOP-C9J4NN0>
2024-11-04 11:02:26 -0500

    Add project files.

---
+++ CentroidAPI_VBnet_XMoveApp/frmMeasureDoor.Designer.vb
@@ -0,0 +1,306 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMeasureDoor
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        tlpButtonContainer = New TableLayoutPanel()
+        gpbMove = New GroupBox()
+        txtDoorMeasurement = New TextBox()
+        btnClear = New Button()
+        btnEnterDecimal = New Button()
+        btnEnter0 = New Button()
+        btnEnter9 = New Button()
+        btnEnter8 = New Button()
+        btnEnter7 = New Button()
+        btnEnter6 = New Button()
+        btnEnter5 = New Button()
+        btnEnter4 = New Button()
+        btnEnter3 = New Button()
+        btnEnter2 = New Button()
+        btnEnter1 = New Button()
+        picCycleCancel = New PictureBox()
+        picCycleStart = New PictureBox()
+        tlpButtonContainer.SuspendLayout()
+        gpbMove.SuspendLayout()
+        CType(picCycleCancel, ComponentModel.ISupportInitialize).BeginInit()
+        CType(picCycleStart, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' tlpButtonContainer
+        ' 
+        tlpButtonContainer.ColumnCount = 3
+        tlpButtonContainer.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 33.3333244F))
+        tlpButtonContainer.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 33.3333359F))
+        tlpButtonContainer.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 33.3333359F))
+        tlpButtonContainer.Controls.Add(gpbMove, 0, 0)
+        tlpButtonContainer.Controls.Add(btnClear, 2, 4)
+        tlpButtonContainer.Controls.Add(btnEnterDecimal, 1, 4)
+        tlpButtonContainer.Controls.Add(btnEnter0, 0, 4)
+        tlpButtonContainer.Controls.Add(btnEnter9, 2, 3)
+        tlpButtonContainer.Controls.Add(btnEnter8, 1, 3)
+        tlpButtonContainer.Controls.Add(btnEnter7, 0, 3)
+        tlpButtonContainer.Controls.Add(btnEnter6, 2, 2)
+        tlpButtonContainer.Controls.Add(btnEnter5, 1, 2)
+        tlpButtonContainer.Controls.Add(btnEnter4, 0, 2)
+        tlpButtonContainer.Controls.Add(btnEnter3, 2, 1)
+        tlpButtonContainer.Controls.Add(btnEnter2, 1, 1)
+        tlpButtonContainer.Controls.Add(btnEnter1, 0, 1)
+        tlpButtonContainer.Location = New Point(10, 9)
+        tlpButtonContainer.Margin = New Padding(4, 3, 4, 3)
+        tlpButtonContainer.Name = "tlpButtonContainer"
+        tlpButtonContainer.RowCount = 5
+        tlpButtonContainer.RowStyles.Add(New RowStyle(SizeType.Percent, 14.49814F))
+        tlpButtonContainer.RowStyles.Add(New RowStyle(SizeType.Percent, 21.37547F))
+        tlpButtonContainer.RowStyles.Add(New RowStyle(SizeType.Percent, 21.3754635F))
+        tlpButtonContainer.RowStyles.Add(New RowStyle(SizeType.Percent, 21.3754635F))
+        tlpButtonContainer.RowStyles.Add(New RowStyle(SizeType.Percent, 21.3754635F))
+        tlpButtonContainer.Size = New Size(436, 426)
+        tlpButtonContainer.TabIndex = 1
+        ' 
+        ' gpbMove
+        ' 
+        tlpButtonContainer.SetColumnSpan(gpbMove, 3)
+        gpbMove.Controls.Add(txtDoorMeasurement)
+        gpbMove.Dock = DockStyle.Fill
+        gpbMove.Location = New Point(3, 3)
+        gpbMove.Name = "gpbMove"
+        gpbMove.Size = New Size(430, 55)
+        gpbMove.TabIndex = 4
+        gpbMove.TabStop = False
+        gpbMove.Text = "Amount to Move X Axis"
+        ' 
+        ' txtDoorMeasurement
+        ' 
+        txtDoorMeasurement.Dock = DockStyle.Bottom
+        txtDoorMeasurement.Font = New Font("Cascadia Mono", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        txtDoorMeasurement.Location = New Point(3, 22)
+        txtDoorMeasurement.Margin = New Padding(4, 3, 4, 3)
+        txtDoorMeasurement.Name = "txtDoorMeasurement"
+        txtDoorMeasurement.Size = New Size(424, 30)
+        txtDoorMeasurement.TabIndex = 2
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Dock = DockStyle.Fill
+        btnClear.Font = New Font("Cascadia Mono", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnClear.Location = New Point(294, 337)
+        btnClear.Margin = New Padding(4, 3, 4, 3)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(138, 86)
+        btnClear.TabIndex = 14
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btnEnterDecimal
+        ' 
+        btnEnterDecimal.Dock = DockStyle.Fill
+        btnEnterDecimal.Font = New Font("Cascadia Mono", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnEnterDecimal.Location = New Point(149, 337)
+        btnEnterDecimal.Margin = New Padding(4, 3, 4, 3)
+        btnEnterDecimal.Name = "btnEnterDecimal"
+        btnEnterDecimal.Size = New Size(137, 86)
+        btnEnterDecimal.TabIndex = 13
+        btnEnterDecimal.Text = "."
+        btnEnterDecimal.UseVisualStyleBackColor = True
+        ' 
+        ' btnEnter0
+        ' 
+        btnEnter0.Dock = DockStyle.Fill
+        btnEnter0.Font = New Font("Cascadia Mono", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnEnter0.Location = New Point(4, 337)
+        btnEnter0.Margin = New Padding(4, 3, 4, 3)
+        btnEnter0.Name = "btnEnter0"
+        btnEnter0.Size = New Size(137, 86)
+        btnEnter0.TabIndex = 12
+        btnEnter0.Text = "0"
+        btnEnter0.UseVisualStyleBackColor = True
+        ' 
+        ' btnEnter9
+        ' 
+        btnEnter9.Dock = DockStyle.Fill
+        btnEnter9.Font = New Font("Cascadia Mono", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnEnter9.Location = New Point(294, 246)
+        btnEnter9.Margin = New Padding(4, 3, 4, 3)
+        btnEnter9.Name = "btnEnter9"
+        btnEnter9.Size = New Size(138, 85)
+        btnEnter9.TabIndex = 11
+        btnEnter9.Text = "9"
+        btnEnter9.UseVisualStyleBackColor = True
+        ' 
+        ' btnEnter8
+        ' 
+        btnEnter8.Dock = DockStyle.Fill
+        btnEnter8.Font = New Font("Cascadia Mono", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnEnter8.Location = New Point(149, 246)
+        btnEnter8.Margin = New Padding(4, 3, 4, 3)
+        btnEnter8.Name = "btnEnter8"
+        btnEnter8.Size = New Size(137, 85)
+        btnEnter8.TabIndex = 10
+        btnEnter8.Text = "8"
+        btnEnter8.UseVisualStyleBackColor = True
+        ' 
+        ' btnEnter7
+        ' 
+        btnEnter7.Dock = DockStyle.Fill
+        btnEnter7.Font = New Font("Cascadia Mono", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnEnter7.Location = New Point(4, 246)
+        btnEnter7.Margin = New Padding(4, 3, 4, 3)
+        btnEnter7.Name = "btnEnter7"
+        btnEnter7.Size = New Size(137, 85)
+        btnEnter7.TabIndex = 9
+        btnEnter7.Text = "7"
+        btnEnter7.UseVisualStyleBackColor = True
+        ' 
+        ' btnEnter6
+        ' 
+        btnEnter6.Dock = DockStyle.Fill
+        btnEnter6.Font = New Font("Cascadia Mono", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnEnter6.Location = New Point(294, 155)
+        btnEnter6.Margin = New Padding(4, 3, 4, 3)
+        btnEnter6.Name = "btnEnter6"
+        btnEnter6.Size = New Size(138, 85)
+        btnEnter6.TabIndex = 8
+        btnEnter6.Text = "6"
+        btnEnter6.UseVisualStyleBackColor = True
+        ' 
+        ' btnEnter5
+        ' 
+        btnEnter5.Dock = DockStyle.Fill
+        btnEnter5.Font = New Font("Cascadia Mono", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnEnter5.Location = New Point(149, 155)
+        btnEnter5.Margin = New Padding(4, 3, 4, 3)
+        btnEnter5.Name = "btnEnter5"
+        btnEnter5.Size = New Size(137, 85)
+        btnEnter5.TabIndex = 7
+        btnEnter5.Text = "5"
+        btnEnter5.UseVisualStyleBackColor = True
+        ' 
+        ' btnEnter4
+        ' 
+        btnEnter4.Dock = DockStyle.Fill
+        btnEnter4.Font = New Font("Cascadia Mono", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnEnter4.Location = New Point(4, 155)
+        btnEnter4.Margin = New Padding(4, 3, 4, 3)
+        btnEnter4.Name = "btnEnter4"
+        btnEnter4.Size = New Size(137, 85)
+        btnEnter4.TabIndex = 6
+        btnEnter4.Text = "4"
+        btnEnter4.UseVisualStyleBackColor = True
+        ' 
+        ' btnEnter3
+        ' 
+        btnEnter3.Dock = DockStyle.Fill
+        btnEnter3.Font = New Font("Cascadia Mono", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnEnter3.Location = New Point(294, 64)
+        btnEnter3.Margin = New Padding(4, 3, 4, 3)
+        btnEnter3.Name = "btnEnter3"
+        btnEnter3.Size = New Size(138, 85)
+        btnEnter3.TabIndex = 5
+        btnEnter3.Text = "3"
+        btnEnter3.UseVisualStyleBackColor = True
+        ' 
+        ' btnEnter2
+        ' 
+        btnEnter2.Dock = DockStyle.Fill
+        btnEnter2.Font = New Font("Cascadia Mono", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnEnter2.Location = New Point(149, 64)
+        btnEnter2.Margin = New Padding(4, 3, 4, 3)
+        btnEnter2.Name = "btnEnter2"
+        btnEnter2.Size = New Size(137, 85)
+        btnEnter2.TabIndex = 4
+        btnEnter2.Text = "2"
+        btnEnter2.UseVisualStyleBackColor = True
+        ' 
+        ' btnEnter1
+        ' 
+        btnEnter1.Dock = DockStyle.Fill
+        btnEnter1.Font = New Font("Cascadia Mono", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnEnter1.Location = New Point(4, 64)
+        btnEnter1.Margin = New Padding(4, 3, 4, 3)
+        btnEnter1.Name = "btnEnter1"
+        btnEnter1.Size = New Size(137, 85)
+        btnEnter1.TabIndex = 3
+        btnEnter1.Text = "1"
+        btnEnter1.UseVisualStyleBackColor = True
+        ' 
+        ' picCycleCancel
+        ' 
+        picCycleCancel.Image = My.Resources.Resources.cycle_cancel
+        picCycleCancel.Location = New Point(14, 438)
+        picCycleCancel.Margin = New Padding(4, 3, 4, 3)
+        picCycleCancel.Name = "picCycleCancel"
+        picCycleCancel.Size = New Size(140, 138)
+        picCycleCancel.SizeMode = PictureBoxSizeMode.Zoom
+        picCycleCancel.TabIndex = 2
+        picCycleCancel.TabStop = False
+        ' 
+        ' picCycleStart
+        ' 
+        picCycleStart.Image = My.Resources.Resources.cycle_start
+        picCycleStart.Location = New Point(303, 438)
+        picCycleStart.Margin = New Padding(4, 3, 4, 3)
+        picCycleStart.Name = "picCycleStart"
+        picCycleStart.Size = New Size(140, 138)
+        picCycleStart.SizeMode = PictureBoxSizeMode.Zoom
+        picCycleStart.TabIndex = 3
+        picCycleStart.TabStop = False
+        ' 
+        ' frmMeasureDoor
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(454, 588)
+        Controls.Add(picCycleStart)
+        Controls.Add(picCycleCancel)
+        Controls.Add(tlpButtonContainer)
+        FormBorderStyle = FormBorderStyle.FixedDialog
+        Margin = New Padding(4, 3, 4, 3)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "frmMeasureDoor"
+        StartPosition = FormStartPosition.CenterParent
+        Text = "Measure Door"
+        tlpButtonContainer.ResumeLayout(False)
+        gpbMove.ResumeLayout(False)
+        gpbMove.PerformLayout()
+        CType(picCycleCancel, ComponentModel.ISupportInitialize).EndInit()
+        CType(picCycleStart, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+    Friend WithEvents tlpButtonContainer As TableLayoutPanel
+    Friend WithEvents btnEnter3 As Button
+    Friend WithEvents btnEnter2 As Button
+    Friend WithEvents btnEnter1 As Button
+    Friend WithEvents btnEnter5 As Button
+    Friend WithEvents btnEnter4 As Button
+    Friend WithEvents btnEnter6 As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnEnterDecimal As Button
+    Friend WithEvents btnEnter0 As Button
+    Friend WithEvents btnEnter9 As Button
+    Friend WithEvents btnEnter8 As Button
+    Friend WithEvents btnEnter7 As Button
+    Friend WithEvents picCycleCancel As PictureBox
+    Friend WithEvents picCycleStart As PictureBox
+    Friend WithEvents gpbMove As GroupBox
+    Friend WithEvents txtDoorMeasurement As TextBox
+End Class
---
+++ CentroidAPI_VBnet_XMoveApp/frmMeasureDoor.vb
@@ -0,0 +1,230 @@
+﻿Imports System.Threading
+Imports CentroidAPI
+Imports CentroidAPI.CNCPipe
+
+'****************************************************************************
+'
+' Title:        frmMeasureDoor.vb
+' Author(s):    Jacob Holes
+' Start Date:   10-31-24
+' Purpose: Custom Application for Customer with custom panel cutting machine
+'
+' Environment/platform  ( Windows 10/11, Visual Basic .net)
+'
+' Last Update:  11-1-24 - JDH - Added error handling
+' Prior Update: 10-31-24 - JDH - Created Project and Solution, added Form, populated form with buttons, wrote logic for X move and Cycle Cancel
+'****************************************************************************
+
+Public Class frmMeasureDoor
+    ' create pipe
+    Dim m_pipe As CNCPipe
+    ' create thread for monitoring if cnc12 is running
+    Dim cnc12_isRunning_thread As Threading.Thread
+
+    ''' <summary>
+    ''' This sub is called when this form is created, we use it to initialize the cnc pipe
+    ''' </summary>
+    Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        ' initialize pipe so we can communicate with CNC12
+        m_pipe = New CentroidAPI.CNCPipe
+    End Sub
+
+    ''' <summary>
+    ''' When the Form Loads, check that the pipe is good, if its not assume CNC12 is not running, inform the user then exit the app. 
+    ''' If we can communicate with CNC12 then start the background thread for checking if CNC12 is running
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub frmMeasureDoor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If m_pipe IsNot Nothing Then
+            If Not m_pipe.IsConstructed Then
+                MessageBox.Show("Could not connect to CNC12. Please make sure CNC12 is running before using this application.", "Could not connect to CNC12")
+                Application.Exit()
+            End If
+        End If
+
+        ' create new thread that calls IsCNC12Running
+        cnc12_isRunning_thread = New Threading.Thread(AddressOf IsCNC12Running)
+        ' set is as a background thread
+        cnc12_isRunning_thread.IsBackground = True
+        ' start the thread
+        cnc12_isRunning_thread.Start()
+    End Sub
+
+    ''' <summary>
+    ''' This sub handles all of the btnEnter[Val] button.click events, it appends the button text to the 
+    ''' txtDoorMeasurment textbox and does not allow users to enter two decimal places
+    ''' </summary>
+    ''' <param name="sender">object which called the sub</param>
+    ''' <param name="e">arguments for event</param>
+    Private Sub HandleButtonPress_Click(sender As Object, e As EventArgs) Handles btnEnter1.Click, btnEnter2.Click, btnEnter3.Click, btnEnter4.Click,
+            btnEnter5.Click, btnEnter6.Click, btnEnter7.Click, btnEnter8.Click, btnEnter9.Click, btnEnter0.Click, btnEnterDecimal.Click
+        ' Get the button the user clicked, cast it as a button
+        Dim ClickedButton As Button = DirectCast(sender, Button)
+        ' if we clicked the decimal point...
+        If ClickedButton.Text = "." Then
+            ' check that we don't already have a decimal point
+            If Me.txtDoorMeasurement.Text.Contains(".") Then
+                ' if we do, don't allow multiple decimal points
+            Else
+                ' otherwise, add the decimal point
+                Me.txtDoorMeasurement.Text = Me.txtDoorMeasurement.Text & ClickedButton.Text
+            End If
+        Else
+            ' allow everything else
+            Me.txtDoorMeasurement.Text = Me.txtDoorMeasurement.Text & ClickedButton.Text
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Clears the text in the txtDoorMeasurement text box
+    ''' </summary>
+    ''' <param name="sender">object which called the sub</param>
+    ''' <param name="e">arguments for event</param>
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        Me.txtDoorMeasurement.Clear()
+    End Sub
+
+    ''' <summary>
+    ''' Sends a G1 command using xDistance as X axis distance and FeedRate as desired FeedRate
+    ''' </summary>
+    ''' <param name="xDistance">Distance to move X Axis</param>
+    ''' <param name="FeedRate">Speed at which to move X Axis</param>
+    ''' <returns></returns>
+    Function SendXMoveCommand(ByVal xDistance As Double, Optional ByVal FeedRate As Double = 50)
+        ' check that pipe is initialized
+        If m_pipe IsNot Nothing Then
+            ' then check that it is constructed
+            If m_pipe.IsConstructed Then
+                ' reference to Job class
+                Dim cmd As New CentroidAPI.CNCPipe.Job(m_pipe)
+                ' return the result of the command after building the command and sending it to cnc12
+                Return cmd.RunCommand("G1 X" & xDistance & " F" & FeedRate, "C:/cncm", False)
+            End If
+        End If
+        ' if we failed, send back an unknown error
+        Return CentroidAPI.CNCPipe.ReturnCode.ERROR_UNKNOWN
+    End Function
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <returns></returns>
+    Function SendCycleStopCommand()
+        If m_pipe IsNot Nothing Then
+            If m_pipe.IsConstructed Then
+                Dim cmd As New CentroidAPI.CNCPipe.Job(m_pipe)
+                Return cmd.CancelExecution()
+            End If
+        End If
+
+        Return CentroidAPI.CNCPipe.ReturnCode.ERROR_UNKNOWN
+    End Function
+
+    ''' <summary>
+    ''' This sub checks if CNC12 is running and updates the title bar text depending on the result
+    ''' </summary>
+    Private Sub IsCNC12Running()
+        ' called from a background thread and we want it to continue forever, so while true...
+        While True
+            Try
+                ' we are testing if we are connected to CNC12 by checking if Parameter 1 has any value.
+                ' If we successfully grab the value, we are connected
+                Dim Param1Val As Double = 0
+                Dim Param As New CentroidAPI.CNCPipe.Parameter(m_pipe)
+                Dim ParamTest As CentroidAPI.CNCPipe.ReturnCode = Param.GetMachineParameterValue(1, Param1Val)
+                ' if we got success then we are connected
+                If ParamTest = CNCPipe.ReturnCode.SUCCESS Then
+                    ' set the title bar text to connected to cnc12
+                    ChangeTitleBarText("Connected to CNC12")
+                ElseIf ParamTest = CNCPipe.ReturnCode.ERROR_PIPE_IS_BROKEN Then
+                    ' if the pipe is broken, try to recreate it.
+                    m_pipe = New CNCPipe
+                Else
+                    ' otherwise assume we are disconnected. 
+                    ChangeTitleBarText("Disconnected from CNC12")
+                End If
+            Catch ex As Exception
+                ' if we get an exception when checking then assume we are disconnected
+                ChangeTitleBarText("Disconnected from CNC12")
+            End Try
+            ' sleep for half a second so we don't use up a CPU core
+            Threading.Thread.Sleep(500)
+        End While
+    End Sub
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub picCycleStart_Click(sender As Object, e As EventArgs) Handles picCycleStart.Click
+        ' don't send an empty string as an x value
+        If Me.txtDoorMeasurement.Text.Trim IsNot "" Then
+            ' get our X value from txtDoorMeasurement.Text as a double
+            Dim XVal As Double = CDbl(Me.txtDoorMeasurement.Text)
+            ' try to send the move command, if it was successful then clear our input box
+            If SendXMoveCommand(XVal) = CentroidAPI.CNCPipe.ReturnCode.SUCCESS Then
+                Me.txtDoorMeasurement.Clear()
+            Else
+                ' if we get any message other than success, then something went wrong. 
+                ' TODO: Add more robust error handling, but this should be fine for this basic example. 
+                MessageBox.Show("An error has occured...")
+            End If
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub picCycleCancel_Click(sender As Object, e As EventArgs) Handles picCycleCancel.Click
+        ' Try to send the cycle stop command, throw a message if we return anything but success
+        If Not SendCycleStopCommand() = CentroidAPI.CNCPipe.ReturnCode.SUCCESS Then
+            MessageBox.Show("An error has occured...")
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' This sub allows us to interact with the title bar text from another thread safely
+    ''' </summary>
+    ''' <param name="text">The text to display in the title bar</param>
+    Private Sub ChangeTitleBarText(ByVal text As String)
+        If Me.InvokeRequired Then
+            Me.BeginInvoke(Sub()
+                               Me.Text = text
+                           End Sub)
+        Else
+            Me.Text = text
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' This sub is called when a key press is detected in the txtDoorMeasurement textbox. It prevents multiple periods from being typed by keyboard.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub txtDoorMeasurement_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtDoorMeasurement.KeyPress
+        ' if using keyboard, don't allow for multiple decimal points
+        If Me.txtDoorMeasurement.Text.Contains(".") Then
+            ' check that our entered character is a period
+            If e.KeyChar = "."c Then
+                ' this prevents the character from being typed
+                e.Handled = True
+            Else
+                ' this allows the character to be typed
+                e.Handled = False
+            End If
+
+        End If
+    End Sub
+End Class
